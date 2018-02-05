308f164728a916bad2f27ec7672e60bc38d244fd
bananaWeb <a89.volkov@gmail.com>
2018-02-05 19:02:45 +0300

    ddd

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 $(document).ready(function (){
-	var input ='323228'
-	$('.js-count').click(function(){
-		showBubble($('.js-firstNumber').val() + $('.js-secondNumber').val());
+	$('.js-plus').click(function(){
+		showBubble(Number(getFirstWord()) + Number(getSecondWord()));
  	});
- 	
- 	function showFirstInput(){
- 		var inputText = $('.js-firstNumber').val();
- 		if (inputText ==="") {
- 			showBubble("TI SHO, DEBIL? ");
- 		} else {
- 			showBubble(inputText);
- 		}
+	$('.js-minus').click(function(){
+		showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+		});
+	$('.js-myltyply').click(function(){
+		showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+		});
+	$('.js-divide').click(function() {
+		if (checkInputData().isNumber) {
+			showBubble(Number(getFirstWord()) / Number(getSecondWord()));
+		} else {
+			if (checkInputData().empty) {
+				showBubble('пуста');
+			} else {
+			showBubble(getFirstWord() + ' ' + getSecondWord()); 
+		}
+	}
+		});
+
+ 	$('.hel').click(function(){
+ 		$('body').addClass("heaven");
+ 	});
+ 	$('.heav').click(function(){
+ 		$('body').removeClass("heaven");
+ 	});
+
+
+ 	//===============================================
+ 	//===============================================
+
+ 	function checkInputData() {
+ 		if($.isNumeric(getFirstWord()) && $.isNumeric(getSecondWord())) {
+ 			return {
+ 				isNumber: true
+ 			};
+ 		} else if (getFirstWord() === '' && getSecondWord() === '') {
+ 			return {
+ 				isNumber:false,
+ 				empty: true
+			}; 
+ 		} else
+ 			return {
+ 				isNumber: false,
+ 				empty: false
+ 		};
  	}
 
+
+	function getFirstWord(){
+ 		return $('.js-firstNumber').val();
+ 	}
+ 	function getSecondWord(){
+ 		return $('.js-secondNumber').val();
+ 	}
  	function showBubble(text) {
 		$('.bubble').text(text);
 		$('.bubble').addClass('-visible');
